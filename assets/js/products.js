3dc919dc7333394a550dba7ba57f2df61a9d4b06
Andrija Antic <andrijant0304@gmail.com>
2023-01-09 16:46:00 +0100

    First commit

---
+++ assets/js/products.js
@@ -0,0 +1,807 @@
+let dbProducts = [
+{
+"imgSrc": "Nadgradnja01",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja02",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja03",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja04",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja05",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja06",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja07",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja08",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nadgradnja09",
+"productTitle": "Nadgradnja vozila"
+
+},
+{
+"imgSrc": "Nameštaj01",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Nameštaj02",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Nameštaj03",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Nameštaj04",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Nameštaj05",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Nameštaj06",
+"productTitle": "Nameštaj"
+
+},
+{
+"imgSrc": "Profili01",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Profili02",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Profili03",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Profili04",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Profili05",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Profili06",
+"productTitle": "Čelični profili"
+
+},
+{
+"imgSrc": "Mašina01",
+"productTitle": "Mašine"
+
+},
+{
+"imgSrc": "Mašina02",
+"productTitle": "Mašine"
+
+},
+{
+"imgSrc": "Ormar01",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar02",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar03",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar04",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar05",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar06",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar07",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar08",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar09",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar10",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar11",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar12",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar13",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar14",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar15",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar16",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar17",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar18",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar19",
+"productTitle": "Ormar"
+
+},
+{
+"imgSrc": "Ormar20",
+"productTitle": "Ormar"
+
+},
+{
+  "imgSrc": "Ormar21",
+  "productTitle": "Ormar"
+  
+},
+{
+  "imgSrc": "Polica01",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica02",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica03",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica04",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica05",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica06",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica07",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica08",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica09",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Polica10",
+  "productTitle": "Polica"
+  
+},
+{
+  "imgSrc": "Roleri01",
+  "productTitle": "Roleri"
+  
+},
+{
+  "imgSrc": "Roleri02",
+  "productTitle": "Roleri"
+  
+},
+{
+  "imgSrc": "Roleri03",
+  "productTitle": "Roleri"
+  
+},
+{
+  "imgSrc": "Roleri04",
+  "productTitle": "Roleri"
+  
+},
+{
+  "imgSrc": "Splav01",
+  "productTitle": "Splav"
+  
+},
+{
+  "imgSrc": "Hranilica01",
+  "productTitle": "Rezervoari"
+  
+},
+{
+  "imgSrc": "Hranilica02",
+  "productTitle": "Hranilice"
+  
+},
+{
+  "imgSrc": "Rezervoari01",
+  "productTitle": "Rezervoari"
+  
+},
+{
+  "imgSrc": "Rezervoari02",
+  "productTitle": "Rezervoari"
+  
+},
+{
+  "imgSrc": "Sto-za-markiranje01",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "Zaštitna-ograda-i-regali01",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img01",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img02",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img03",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img04",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img05",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img06",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img07",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img08",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img09",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "img10",
+  "productTitle": "razno"
+  
+},
+{
+  "imgSrc": "Gromobranija01",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija02",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija03",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija04",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija05",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija06",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija07",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija08",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija09",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija10",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija11",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija12",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija13",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija14",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija15",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija16",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija17",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija18",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija19",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija20",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija21",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija22",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija23",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija24",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija25",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija26",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija27",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija28",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija29",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija30",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija31",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "Gromobranija32",
+  "productTitle": "Gromobranija"
+  
+},
+{
+  "imgSrc": "img11",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img12",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img13",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img14",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img15",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img16",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img17",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img18",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img19",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img20",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img21",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img22",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img23",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img24",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img25",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img26",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img27",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img28",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img29",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img30",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img31",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img32",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img33",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img34",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img35",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img36",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img37",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img38",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img39",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img40",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img41",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img42",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img43",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img44",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img45",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img46",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+},
+{
+  "imgSrc": "img47",
+  "productTitle": "Bravarski-radovi-čelične-konstrukcije"
+  
+}
+
+]
+
+// function loadData() {
+//   let xml = new XMLHttpRequest();
+//   let url = "products.json";
+//   xml.open('GET', url);
+//   xml.onreadystatechange = function() {
+//     if (xml.readyState === 4 && xml.status === 200) {
+//       createProductsData(JSON.parse(xml.responseText));
+      
+//     }
+//   }
+//   xml.send();
+// };
+// loadData();
+//assets\images\products\camping-gd4ccd3cd5_640.jpg
+
+
+let products = document.querySelector('#products-container');
+let templateProducts = ``;
+
+// function createProductsData() {
+  
+//  //let dbProducts = data;
+
+for (let i = 0; i < dbProducts.length; i++) {    
+const user = dbProducts[i];
+  templateProducts +=`
+    <div class="product-card col-lg-3 col-md-6 mb-4">
+      <div class="card m-auto img-fluid">
+        <img class="gallery-img" src="../assets/images/products/${user.imgSrc}.jpg" alt="">
+        
+        
+      </div>
+    </div>
+    `
+  };
+  products.innerHTML = templateProducts;  
+  
+
+//   function enlargeImage() {
+//     document.getElementById("product-image").src = this.src;
+//     document.getElementById("product-title").innerHTML = this.alt;
+//     document.getElementById('myModal').style.display = "block";
+//   }
+  
+//   (function() {
+//     var images = document.getElementsByClassName("myImg");
+//     for (i = 0; i < images.length; i++) {
+//       images[i].onclick = enlargeImage;
+//     }
+//   })();
+
+//   var modal = document.querySelector("#myModal");
+//   var img = document.querySelector(".myImg");
+//   var modalImg = document.querySelector(".modal-image");
+//   var productTitle = document.querySelector("#product-title");
+  
+//   img.addEventListener('click', openModal);
+  
+//   function openModal(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     productTitle.innerHTML = this.alt;
+//   }
+
+  
+//   var span = document.getElementsByClassName("close")[0];
+  
+//   span.addEventListener('click', closeModal);
+
+//   function closeModal() {
+//     modal.style.display = "none";
+//   }
+
+
+//   window.addEventListener('click', outsideClick);
+
+//   function outsideClick(e) {
+//     if (e.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+  
+//};
